0c35bc852bcfce457775cd9976946409117713df
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2016-06-15 14:53:43 -0300

    test_commit

--- src/pdi/PDITeste.java
+++ src/pdi/PDITeste.java
@@ -3,18 +3,20 @@ import images.*;
 
 public class PDITeste {
 
-	public static String PATH = "/home/bandeira/Documents/university/2016.1/pdi/workspace/DocumentBinary/src/images/";
+//	public static String PATH = "/home/bandeira/Documents/university/2016.1/pdi/workspace/DocumentBinary/src/images/";
+	public static String PATH = "/Users/Vinicius/Documents/workspace/pdi/src/images/";
 	public static String INPUTEXT = ".png";
 	public static String OUTPUTEXT = ".jpg";
 	public static void main(String[] args) {
 		
 		
-		String filename = "";
-		int windowRadius = 10;		
+		String filename = "H01";
+		int windowRadius = 7;		
 		double[][][] colorida = PDI.lerImagemColorida(PATH + filename + INPUTEXT);
 		double[][] cinza = PDI.retornaImagemCinza(colorida);
-		//PDI.niblackMethod(cinza, 1);
-		PDI.salvaImagem(PATH + filename + "_gray_otsu" + OUTPUTEXT, PDI.binaryImage(cinza));
+//		PDI.niblackMethod(cinza, 1);
+//		PDI.salvaImagem(PATH + filename + "_gray_otsu" + OUTPUTEXT, PDI.binaryImage(cinza));
+		PDI.salvaImagem(PATH + filename + "_gray_niblack" + OUTPUTEXT, PDI.niblackMethod(cinza,windowRadius));
 		//PDI.salvaImagem(path + filename + "_gray_niblack_" + windowRadius + "_" + outputExt, PDI.niblackMethod(cinza, windowRadius));
 
 	}
